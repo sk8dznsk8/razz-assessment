61e3ef7793be1ea8d95065359d656136b6e1a8bd
Eduardo <egoliveira.ti@gmail.com>
2020-01-31 10:23:57 -0500

    Mongo Client message fixed

--- src/db/mongodb.js
+++ src/db/mongodb.js
@@ -1,7 +1,7 @@
 const { MongoClient } = require('mongodb');
 
 const initialize = async () => {
-    return await MongoClient.connect(process.env.MONGODB_URL, { useNewUrlParser: true });
+    return await MongoClient.connect(process.env.MONGODB_URL, { useNewUrlParser: true, useUnifiedTopology: true });
 }
 
 module.exports = initialize;
